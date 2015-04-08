3dfebe7c640fd76dc1731225485de87dd66504ef
Arjun Kalburgi <askalburgi@gmail.com>
2015-04-08 13:33:50 -0600

    a few changes for cleaner code

--- app/views.py
+++ app/views.py
@@ -7,6 +7,8 @@ from game.Board import board as b
 from game.connectfour import SBM as comp
 g = b()
 
+
+# routes for startup, need to render start.html
 @app.route("/")
 @app.route("/startup/", methods=['GET', 'POST'])
 def start(): 
@@ -14,50 +16,61 @@ def start():
 							title='Connect4')
 
 
+# route for the back page, need to render credits.html 
 @app.route("/more/")
 def credits(): 
 	return render_template('credits.html', 
 							title='Connect4')
 
 
-
-
+# route and ajax functions for game page
+# takes care of rendering the template, 
 @app.route('/game/', methods=["GET", "POST"])
 def game(): 
 	return render_template('game.html', 
 							title='Connect4')
 
+# what to do when the user must play, 
 @app.route('/user_play/', methods=['POST'])
 def user_play(): 
+	# gets the column that the user wants
 	user_col = request.form['col']
+
+	# adds a user piece to that column
 	g.add_piece(2, int(user_col))
+
+	# returns a json string of the new board
 	return jsonify(board=g.board)
 
+# what to do to check if there is a winner, 
 @app.route('/check_win/', methods=['POST']) 
 def check_win(): 
+	# returns a json of the result of check_win()
 	return jsonify(player=g.check_win())
 
+# what to do when the computer must play, 
 @app.route('/comp_play/', methods=['POST'])
 def comp_play(): 
 	# get the parameters 
 	difficulty = int(request.form['val'])
-	# memo = eval(request.form['memo'])
 
 	# save the board 
 	p = copy.deepcopy(g)
 
 	# find the computers best way
-	# q, memo = comp(p, difficulty,1,memo)
 	q = comp(p, difficulty)
 
 	# add it to the board
 	g.add_piece(1,q)
 
+	# returns a json string of the new board
 	return jsonify(board=g.board)
-	# return jsonify({'board':g.board,'m':str(memo)})
 
+# and what to do when the game is restarted
 @app.route('/restart/', methods=['POST'])
 def restart():
+	# clears the board
 	g.clear_board()
-	# g.print_board()
+
+	# returns a json string of the new board
 	return jsonify(board=g.board)
--- game/Board.py
+++ game/Board.py
@@ -1,5 +1,14 @@
 class board: 
 	def __init__(self):
+		'''
+		define a board as having 7 columns, 7 rows 
+		and a board that is a tuple of lists. 
+
+		Initializing it as a tuple of lists means that
+		we won't be able to change the tuple's elements by
+		accident, but we can change the list's elements in
+		order to manipulate the board.
+		'''
 		self.columns = 7
 		self.rows = 7
 
@@ -12,11 +21,21 @@ class board:
 					  [0,0,0,0,0,0,0])
 
 	def print_board(self): 
+		'''
+		prints the board nicely
+		'''
 		i = 0
 		for i in self.board:
 			print(i)
 
 	def add_piece(self, player, col): 
+		'''
+		Arguments: The player, and the column
+
+		Function: add_piece puts the piece of the 
+		player (in the game the computer is 1 and the 
+		user is 2) at the lowest available spot in the column 
+		'''
 		for i, row in enumerate(self.board):
 			if i+1 == 7: 
 				row[col] = player
@@ -26,12 +45,20 @@ class board:
 				break
 
 	def rm_piece(self, col): 
+		'''
+		Takes the topmost piece of a column out
+		'''
 		for i in range(0,7): 
 			if self.board[i][col] is not 0: 
 				self.board[i][col] = 0
 				break
 
 	def available_cols(self): 
+		'''
+		Returns a list of all the columns that are 
+		not full. The function only checks the top row 
+		of the board.
+		'''
 		av_cols = list()
 		for i,col in enumerate(self.board[0]): 
 			if col is 0: 
@@ -46,7 +73,7 @@ class board:
 
 		This Heuristic Function is set up on the logic that setting or breaking
 		up a ConnectFour is highest priority. Next is setting up or breaking up 
-		a three-ina-row. Next is close to the previous players turn. 
+		a three-in-a-row. Next is close to the previous players turn. 
 		'''
 		# get place of where piece would go. 
 		for i, row in enumerate(self.board):
@@ -55,30 +82,30 @@ class board:
 				# row is the row of our spot
 				# i is the index of the row
 
-
-		# # check for Connect Four
-		# if checkfour(row, col, i, self.board): 
-		# 	return 10
-
-		# check for Connect Three
-		# if checkthree(row, col, i, self.board): 
-		# 	return 8
-
+		# check for any three in a row
 		h = checkthree2(row, col, i, self.board)
 		if h: 
-			# if I'm playing as two and the 
-			# connect line I'm making is for two
-			# Then that means, two will win, so mark that 
-			# very low
+			# if playing as p2 and the connect line 
+			# is a line for p2, then two will win.
+			# this case should be marked low.
 			if h == p == 2:
 				return -8
 			else: 
 				return 8 
 
-
 		return 0
 
 	def check_win(self): 
+		'''
+		Function looks for a four in three possible scenarios:
+		in a row, in a column and diagonally. To check diagonally, 
+		it needs to look diagonally both ways (up to the left and 
+		up to the right). However to avoid trying to access elements
+		that don't exist, the function goes through the first 3 elements 
+		in the columns and in the rows. This increases the number of cases
+		by needing to check down to the left and and down to the right to
+		make sure to get them all. 
+		'''
 		# check row
 		for row in self.board: # rows
 			for i in range(0,4): # columns
@@ -115,6 +142,9 @@ class board:
 		return False
 
 	def clear_board(self): 
+		'''
+		simply resets the board to all 0's
+		'''
 		self.board = ([0,0,0,0,0,0,0],
 					  [0,0,0,0,0,0,0],
 					  [0,0,0,0,0,0,0],
@@ -124,6 +154,9 @@ class board:
 					  [0,0,0,0,0,0,0])
 
 	def immutable(self):
+		'''
+		returns the tuple of lists as a tuple of tuples. 
+		'''
 		return (tuple(self.board[0]), tuple(self.board[1]), tuple(self.board[2]), tuple(self.board[3]), \
 			 tuple(self.board[4]), tuple(self.board[5]), tuple(self.board[6]))
 
@@ -152,135 +185,6 @@ class board:
 			return False
 		else: 
 			return True
-
-
-def checkthree(row, col, i, b): 
-	if col == 0: 
-		# check for three flat in the row. 
-		if row[col+1] == row[col+2]!=0:
-			return True
-		# check for three diagonal
-		if i > 2 and twoupright(b, i, col):
-			return True
-		if i < 4 and twodownright(b, i, col): 
-			return True
-	if col == 1: 
-		# check for three flat in the row. 
-		if row[col+1] == row[col+2]!=0 or \
-		 row[col-1] == row[col+1]!=0:
-			return True
-		# check for three diagonal
-		if i > 1 and twoupright(b, i, col):
-			return True
-		if i < 5 and twodownright(b, i, col): 
-			return True
-		if i > 0 and i < 5 and leftuprightdown(b,i,col): 
-			return True
-		if i < 6 and i > 1 and rightupleftdown(b,i,col): 
-			return True
-	if col == 2: 
-		# check for three flat in the row. 
-		if row[col+1] == row[col+2]!=0 or \
-		 row[col-1] == row[col+1]!=0 or \
-		 row[col-2] == row[col-1]!=0: 
-			return True
-		# check for three diagonal
-		if i > 1 and i < 5 and twoupleft(b, i, col): 
-			return True
-		if i < 5 and i > 1 and twodownleft(b, i, col): 
-			return True
-		if i > 1 and twoupright(b, i, col):
-			return True
-		if i < 5 and twodownright(b, i, col): 
-			return True
-		if i > 0 and i < 6 and (leftuprightdown(b,i,col) or rightupleftdown(b,i,col)): 
-			return True
-	if col == 3:
-		# check for three flat in the row. 
-		if row[col+1] == row[col+2]!=0 or \
-		 row[col-1] == row[col+1]!=0 or \
-		 row[col-2] == row[col-1]!=0 or \
-		 row[col-3] == row[col-2]!=0: 
-			return True
-		# check for three diagonal
-		if i > 1 and twoupleft(b, i, col) and twoupright(b, i, col): 
-			return True
-		if i < 5 and twodownleft(b, i, col) and twodownright(b, i, col): 
-			return True
-		if i > 0 and i < 6 and (leftuprightdown(b,i,col) or rightupleftdown(b,i,col)): 
-			return True
-	if col == 4: 
-		# check for three flat in the row. 
-		if row[col-1] == row[col-2]!=0 or \
-		 row[col+1] == row[col-1]!=0 or \
-		 row[col+2] == row[col+1]!=0: 
-			return True
-		# check for three diagonal
-		if i > 1 and i < 5 and twoupright(b, i, col): 
-			return True
-		if i < 5 and i > 1 and twodownright(b, i, col): 
-			return True
-		if i > 1 and twoupleft(b, i, col):
-			return True
-		if i < 5 and twodownleft(b, i, col): 
-			return True
-		if i > 0 and i < 6 and (leftuprightdown(b,i,col) or rightupleftdown(b,i,col)): 
-			return True
-	if col == 5: 
-		# check for three flat in the row. 
-		if row[col-1] == row[col-2]!=0 or \
-		 row[col+1] == row[col-1]!=0:
-			return True
-		# check for three diagonal
-		if i > 1 and twoupleft(b, i, col):
-			return True
-		if i < 5 and twodownleft(b, i, col):
-			return True
-		if i > 0 and i < 5 and rightupleftdown(b,i,col): 
-			return True
-		if i < 6 and i > 1 and leftuprightdown(b,i,col): 
-			return True
-	if col == 6: 
-		# check for three flat in the row. 
-		if row[col-1] == row[col-2]!=0:
-			return True
-		# check for three diagonal 
-		if i > 2 and twoupleft(b, i, col):
-			return True
-		if i < 4 and twodownleft(b, i, col): 
-			return True
-	
-	# check for three vertical 
-	if i < 4: 
-		if b[i+1][col] == b[i+2][col]!=0: 
-			return True 
-
-	return False
-
-def twoupright(b, i, col): 
-	if b[i-1][col+1] == b[i-2][col+2]!=0: 
-		return True
-
-def twodownright(b, i, col): 
-	if b[i+1][col+1] == b[i+2][col+2]!=0: 
-		return True
-
-def twoupleft(b, i, col): 
-	if b[i-1][col-1] == b[i-2][col-2]!=0: 
-		return True
-
-def twodownleft(b, i, col): 
-	if b[i+1][col-1] == b[i+2][col-2]!=0:
-		return True
-
-def leftuprightdown(b,i,col): 
-	if b[i+1][col+1] == b[i-1][col-1]!=0: 
-		return True
-
-def rightupleftdown(b,i,col): 
-	if b[i+1][col-1] == b[i-1][col+1]!=0: 
-		return True
-
 
 
 def checkthree2(row, col, i, b): 
